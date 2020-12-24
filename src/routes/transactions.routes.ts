33062b3f501fbc1b95e082a5fbc45c02ac9905ae
SAMUEL APOLINARIO ARAO <samuel.arao@gmail.com>
2020-12-24 13:04:26 -0300

    Agorafoi

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -9,7 +9,10 @@ import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
 
 
-import upload from '../config/upload';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -47,7 +50,9 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', upload.single('file'),async (request, response) => {
+transactionsRouter.post('/import', 
+  upload.single('file'),
+  async (request, response) => {
     const importTransactions = new ImportTransactionsService();
 
     const transactions = await importTransactions.execute(request.file.path);

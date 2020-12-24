a115c8cd48411f55d0031c1b5117b9d8be3a061c
SAMUEL APOLINARIO ARAO <samuel.arao@gmail.com>
2020-12-24 12:54:52 -0300

    Final

---
+++ src/database/migrations/1607262481885-CreateCategory.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCategory1607262481885 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.createTable(
+            new Table({
+            name: 'category',
+            columns: [
+                    { 
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                    },
+                    {
+                        name: 'title', 
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropTable('category');
+    }
+
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,61 @@
 // import AppError from '../errors/AppError';
+import {getCustomRepository, getRepository} from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import transactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface Request{
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category
+  } : Request): Promise<Transaction> {
+    const transactionsRepositories = getCustomRepository(transactionsRepository);
+
+    const categoriesRepository = getRepository(Category);
+
+
+    const { total } = await transactionsRepositories.getBalance();
+
+    if(type == 'outcome' && total < value){
+      throw new AppError('You do not have enough balance');
+    }
+
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where:{
+        title: category,
+      }
+    });
+    
+    if(!transactionCategory){
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepositories.create({
+      title,
+      type,
+      value,
+      category: transactionCategory
+    });
+
+    await transactionsRepositories.save(transaction);
+
+    return transaction;
   }
 }
 
